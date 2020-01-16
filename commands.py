cfe0030f756ac392d95f4b708b79c36fbcfec000
Fitti <dev@fitti.io>
2020-01-16 01:04:38 +0100

    Add easter egg

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import functools
 from datetime import datetime
 import json
+import random
 
 from get_user_info import get_uid
 
@@ -187,6 +188,11 @@ class Commands:
     @check_permissions
     @check_cooldown(cooldown=30)
     def on_pat(self, e, msg, c, bot):
+        randnr = random.randint(1, 100)
+        if randnr == 1:
+            c.privmsge(bot.channel, "TRICKED YOU! It's Egypt" +
+                       " and we're both phlog pyros!")
+            return
         with open('pat_count.json', 'r') as patfile:
             patlist = json.load(patfile)
             patnum = patlist.pop()
